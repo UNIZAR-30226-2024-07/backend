138e9ca3ad84ff32d688f7b3557646dacbeff544
Flavio-Tolosana <flavio.tolosana@gmail.com>
2024-03-25 15:09:24 +0000

    Rug y Card controllers terminados

--- controllers/rugController.js
+++ controllers/rugController.js
@@ -1,75 +1,13 @@
 // Imports de esquemas necesarios
+const { dirUploads } = require("../config")
 const Rug = require("../models/rugSchema")
-
-// Función para agregar un nuevo rug
-const add = async (req, res) => {
-    try {
-        const r = req.body
-        // Verificar si image y price están presentes y no son vacíos, error
-        if (!r.image || !r.price || r.image.trim() === "" || r.price.trim() === "") {
-            return res.status(404).json({
-                status: "error",
-                message: "Parámetros enviados incorrectamente. Se deben incluir los campos: image, price"
-            })
-        }
-
-        // Si existe rug con misma image, error
-        const oldRug = await Rug.findOne({ image: r.image })
-        if (oldRug) {
-            return res.status(404).json({
-                status: "error",
-                message: "Ya existe un rug con esa image"
-            })
-        }
-
-        // Crear rug, exito
-        const newRug = await Rug.create({ image: r.image, price: r.price });
-        res.status(200).json({
-            status: "success",
-            message: "Rug creado correctamente",
-            rug: newRug
-        });
-    } catch (error) {
-        return res.status(404).json({
-            status: "error",
-            message: error.message
-        })
-    }
-}
-
-// Función para eliminar un rug por su ID
-const eliminate = async (req, res) => {
-    try {
-        const id = req.params.id
-
-        // Encontrar y eliminar rug por id
-        const rug = await Rug.findByIdAndDelete(id)
-
-        // Rug no encontrado, error
-        if (!rug) {
-            return res.status(404).json({
-                status: "error",
-                message: "Rug no encontrado"
-            })
-        } else {  // Rug encontrado, exito
-            return res.status(200).json({
-                status: "success",
-                message: "Rug eliminado correctamente"
-            })
-        }
-
-    } catch (error) {
-        return res.status(404).json({
-            status: "error",
-            message: error.message
-        })
-    }
-}
+const fs = require('fs')
+const path = require('path')
 
 // Función para actualizar un rug por su ID
 const update = async (req, res) => {
     try {
-
+        
         const id = req.params.id
         const r = req.body
 
@@ -145,10 +83,115 @@ const rugById = async (req, res) => {
     }
 }
 
+// Función para obtener todos los ruges
+const  getAllRugs = async (req, res) => {
+    try {
+        // Consulta todos los ruges en la base de datos
+        const ruges = await Rug.find()
+        // Ruges no esncontrados, error
+        if (!ruges) {
+            return res.status(404).json({
+                status: "error",
+                message: "Ruges no encontrados"
+            })
+        } else {   // Ruges encontrados, exito
+            res.status(200).json({
+                status: "success",
+                message: "Ruges obtenidos correctamente",
+                rug: ruges
+            })
+        }
+    } catch (error) {
+        return res.status(404).json({
+            status: "error",
+            message: error.message
+        })
+    }
+}
+
+// Funciones exclusivas del administrador
+
+// Función para agregar un nuevo rug
+const add = async (req, res) => {
+    try {
+        const r = req.body
+        const imageFileName = req.file.filename;
+        
+        // Verificar si image y price están presentes y no son vacíos, error
+        if (!r.image || !r.price || r.image.trim() === "" || r.price.trim() === "") {
+            return res.status(404).json({
+                status: "error",
+                message: "Parámetros enviados incorrectamente. Se deben incluir los campos: image, price"
+            })
+        }
+
+        // Si existe rug con misma image, error
+        const oldRug = await Rug.findOne({ image: r.image })
+        if (oldRug) {
+            return res.status(404).json({
+                status: "error",
+                message: "Ya existe un rug con esa image"
+            })
+        }
+
+        // Crear rug, exito
+        const newRug = await Rug.create({ image: r.image, price: r.price, imageFileName: imageFileName });
+        res.status(200).json({
+            status: "success",
+            message: "Rug creado correctamente",
+            rug: newRug
+        });
+    } catch (error) {
+        return res.status(404).json({
+            status: "error",
+            message: error.message
+        })
+    }
+}
+
+// Función para eliminar un rug por su ID
+const eliminate = async (req, res) => {
+    try {
+        const id = req.params.id
+        
+        // Encontrar y eliminar rug por id
+        const rug = await Rug.findByIdAndDelete(id)
+        
+        // Rug no encontrado, error
+        if (!rug) {
+            return res.status(404).json({
+                status: "error",
+                message: "Rug no encontrado"
+            })
+        }
+        // Rug eliminado encontrado, exito
+        // Eliminar imagen servidor
+        const imagePath = path.join(__dirname, `../${dirUploads}/`, rug.imageFileName);
+        fs.unlink(imagePath, (err) => {
+            if (err) {
+                return res.status(404).json({
+                    status: "error",
+                    message: "Error al eliminar foto del servidor (rug si está eliminado de la base de datos)"
+                })
+            }
+        })
+        return res.status(200).json({
+            status: "success",
+            message: "Rug eliminado correctamente"
+        })
+    } catch (error) {
+        return res.status(404).json({
+            status: "error",
+            message: error.message
+        })
+    }
+}
+
 // Funciones que se exportan
 module.exports = {
     add,
     eliminate,
     update,
-    rugById
+    rugById,
+    getAllRugs
 }
